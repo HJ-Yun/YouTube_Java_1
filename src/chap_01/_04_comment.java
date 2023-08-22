79629c9265f51597d2fc4258a3d0a6c35146c642
Hyeokjin Yun <hjyun6130@gmail.com>
2023-08-23 00:02:55 +0900

    second commit

--- src/chap_01/_04_comment.java
+++ src/chap_01/_04_comment.java
@@ -3,15 +3,18 @@ package chap_01;
 public class _04_comment {
     public static void main(String[] args) {
         System.out.println("(10min) before start the wedding.");
-        System.out.println("(5min) before start the wedding.");
+        //System.out.println("(5min) before start the wedding.");
         System.out.println("now start the wedding.");
 
         int size = 120;
         size = size + 10; //
-        System.out.println(size);
+        System.out.println("Please let me see size " + size); // Reason that why plus 10 using with comment
 
-        int a = 10;
+        /*int a = 10;
         int b = 20;
-        System.out.println(a + b);
+        System.out.println(a + b);*/
+
+        // comments for one line : Ctrl + /
+        // comments for multiple : Ctrl + Shift + /
     }
 }
--- src/chap_01/_05_valiable.java
+++ src/chap_01/_05_valiable.java
@@ -2,7 +2,15 @@ package chap_01;
 
 public class _05_valiable {
     public static void main(String[] args) {
-        String nataional = "korea";
+        /*Variable for name
+         1. Reasonable name
+         2. UnderBar(_), Character(a,b,c), Number(1,2,3), can be used. (No blank)
+         3. Can start from UnderBar or Character
+         4. One or more Character
+         5. Start from small character, each of word starting from large character
+         */
+
+        String nationality = "korea";
         String firstName = "jin";
         String lastName = "yun";
         String cityOfKorea = "Deagu";
@@ -13,7 +21,7 @@ public class _05_valiable {
         String item1 = "watch";
         String item2 = "doll";
 
-        final String CODE = "KR";
+        final String CODE = "KR"; //The constant must use large character
 
 
 
